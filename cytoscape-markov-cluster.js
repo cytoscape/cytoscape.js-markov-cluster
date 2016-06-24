fb57c51134cf4cec5c2745e15c4b8f19338fb4d5
Zoe Xi <email@zoexi.com>
2016-06-24 14:45:26 -0700

    generate matrix from input graph

--- cytoscape-markov-cluster.js
+++ cytoscape-markov-cluster.js
@@ -1,10 +1,15 @@
 ;(function(){ 'use strict';
 
   var defaults = {
-    expandFactor: 2,
-    inflateFactor: 2,
-    multFactor: 1,
-    maxIterations: 10
+    expandFactor: 2,      //
+    inflateFactor: 2,     //
+    multFactor: 1,        // self loops for each node
+    maxIterations: 10,    //
+    attributes: [
+        function(edge) {
+          return 1;
+        }
+    ]
   };
 
   var setOptions = function( opts, options ) {
@@ -12,16 +17,59 @@
     for (var i in options)  { opts[i] = options[i];  }
   };
 
+  var printMatrix = function( M ) { // used for debugging purposes only
+    var n = Math.sqrt(M.length);
+    for ( var i = 0; i < n; i++ ) {
+      var row = '';
+      for ( var j = 0; j < n; j++ ) {
+        row += M[i*n+j] + ' ';
+      }
+      console.log(row);
+    }
+  };
+
+  var getSimilarity = function( edge, attributes ) {
+    var total = 0;
+    for ( var i = 0; i < attributes.length; i++ ) {
+      total += attributes[i]( edge );
+    }
+    return total;
+  };
+
   var hasConverged = function( M, iterations ) {
 
   };
 
   var markovCluster = function( options ) {
-    var eles = this;
+    var nodes = this.nodes();
+    var edges = this.edges();
     var cy = this.cy();
+    var opts = {};
 
     // Set parameters of algorithm:
     setOptions( opts, options );
+
+    // Map each node to its position in node array
+    var id2position = {};
+    for( var i = 0; i < nodes.length; i++ ){
+      id2position[ nodes[i].id() ] = i;
+    }
+
+    // Generate stochastic matrix M from input graph G (should be symmetric/undirected)
+    var n = nodes.length, n2 = n * n;
+    var M = new Array( n2 );
+    for (var i = 0; i < n2; i++) {
+      M[i] = 0;
+    }
+
+    for ( var e = 0; e < edges.length; e++ ) {
+      var edge = edges[e];
+      var i = id2position[ edge.source().id() ];
+      var j = id2position[ edge.target().id() ];
+      M[i * n + j] += getSimilarity( edge, opts.attributes ); // G should be symmetric and undirected
+    }
+    //printMatrix( M );
+    //debugger;
 
     // Begin Markov cluster algorithm
     var clusters = [];
